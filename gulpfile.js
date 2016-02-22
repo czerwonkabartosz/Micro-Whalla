cab69fadfb961cb842cc8a50d03ac513729cf95d
Bartosz Czerwonka <czerwonka.bartosz@gmail.com>
2016-02-22 13:33:39 +0100

    Add istanbul for code coverage
    - add task to gulp to check code coverage

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,23 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var istanbul = require('gulp-istanbul');
 var eslint = require('gulp-eslint');
 var runSequence = require('run-sequence');
 
-gulp.task('test', ['lint'], function () {
-  return gulp.src('test/*.test.js', { read: false })
-    .pipe(mocha());
+gulp.task('pre-test', function () {
+  return gulp.src(['lib/**/*.js'])
+    .pipe(istanbul({ includeUntested: true }))
+    .pipe(istanbul.hookRequire());
+});
+
+gulp.task('test', ['lint', 'pre-test'], function () {
+  return gulp.src('test/*.test.js')
+    .pipe(mocha())
+    .pipe(istanbul.writeReports());
 });
 
 gulp.task('lint', function () {
-  return gulp.src(['**/*.js', '!node_modules/**'])
+  return gulp.src(['**/*.js', '!node_modules/**', '!coverage/**'])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
